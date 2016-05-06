0f79f0bbf9e243fe3ea401702682193857eb16b5
levinnir93 <levinnir93@gmail.com>
2016-05-06 17:59:29 +0300

    no message

--- Moodleapp/app/src/main/java/Moodle/Moodle.java
+++ Moodleapp/app/src/main/java/Moodle/Moodle.java
@@ -1,6 +1,4 @@
 package Moodle;
-
-import java.util.LinkedList;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +8,7 @@ import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 /**
@@ -17,43 +16,44 @@ import org.json.JSONObject;
  */
 public class Moodle {
 
-    private Course[] courses; // all courses
-   // private boolean signedIn;
+    //private Course[] courses; // all courses
+    // private boolean signedIn;
 
-    public static void Login(String username, String password)  {
+    public static void Login(String username, String password) {
         //signedIn = false;
-       // if (!signedIn) {
-            String URL = "http://moodletest.com/login/token.php?username=" + username + "&password=" + password + "&service=moodle_mobile_app";
-            JSONObject json;
+        // if (!signedIn) {
+        String URL = "http://moodletest.com/login/token.php?username=" + username + "&password=" + password + "&service=moodle_mobile_app";
+        JSONObject json;
+        String token;
         try {
             json = readJsonFromUrl(URL);
-            System.out.println(json.toString(4));
+            token = extractToken(json.toString(4));
+            System.out.println(token);
+
+            // use token to get get function
+            String function = "mod_assign_get_assignments";
+            URL = "http://moodletest.com/webservice/rest/server.php?wstoken=" + token + "&moodlewsrestformat=json&wsfunction=" + function;
+            json = readJsonFromUrl(URL);
+            JSONArray assignments = json.getJSONArray("courses").getJSONArray(0);
+            // Put into Assignments and Courses
+            System.out.print(json.toString());
+
         } catch (java.io.IOException e) {
             System.out.println("Error : " + e.getMessage());
         } catch (org.json.JSONException e) {
             System.out.println("Error : " + e.getMessage());
         }
-       // }
+        // }
     }
 
     public void Logout() {
-        courses = null;
+        //courses = null;
         //signedIn = false;
     }
 
-    public List<Course> getCousrses() {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    public List<Assignment> getAllAssignments() {
-        List<Assignment> assigns = new LinkedList<>();
-
-        assigns.add(new Assignment());
-        assigns.add(new Assignment());
-        assigns.add(new Assignment());
-
-        return assigns;
-    }
+   /* public List<Assignment> getAllAssignments() {
+        return null;
+    }*/
 
     private static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
         InputStream is = new URL(url).openStream();
@@ -74,6 +74,15 @@ public class Moodle {
             sb.append((char) cp);
         }
         return sb.toString();
+    }
+
+    public static String extractToken(String fullToken) {
+        int colon = fullToken.indexOf(":");
+        String almostToken = fullToken.substring(colon + 3);
+        int endingQuote = almostToken.indexOf("\"");
+        String token = almostToken.substring(0, endingQuote);
+        return token;
+
     }
 
     public static void main(String[] args) {
