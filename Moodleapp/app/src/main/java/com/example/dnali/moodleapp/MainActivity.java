e7bdbe7c29ea0565ab86f894ba2177b7a5178954
Daniel Alima <Daniel Alima>
2016-05-06 10:18:51 +0300

    new login screen

---
+++ Moodleapp/app/src/main/java/com/example/dnali/moodleapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dnali.moodleapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText textUsername;
+    private EditText textPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textUsername = (EditText)findViewById(R.id.ET_username);
+        textPassword = (EditText) findViewById(R.id.ET_password);
+        Button login = (Button) findViewById(R.id.b_login);
+
+       login.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+               //              intent.putExtra(SUCCESS_KEY, isSuccess());
+               startActivity(intent);
+               overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+           }
+       });
+    }
+
+
+}
---
+++ Moodleapp/app/src/main/java/com/example/dnali/moodleapp/ResultActivity.java
@@ -0,0 +1,7 @@
+package com.example.dnali.moodleapp;
+
+/**
+ * Created by dnali on 5/5/2016.
+ */
+public class ResultActivity {
+}
