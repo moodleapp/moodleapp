c88f5d4793aa44738cfd9263c9f6c6e6e7542b4c
shahaf.benyakir <shahaf@h-MacBook-Pro-ofshahaf.local>
2016-05-06 17:57:34 +0300

    Moving moodle instance between activities complete

--- Moodleapp/app/src/main/java/com/example/dnali/moodleapp/taskActivity.java
+++ Moodleapp/app/src/main/java/com/example/dnali/moodleapp/taskActivity.java
@@ -1,31 +1,61 @@
 package com.example.dnali.moodleapp;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 
+import java.util.LinkedList;
 import java.util.List;
 
-import Moodle.Assignment;
+import MoodleDemo.Assignment;
+import MoodleDemo.Moodle;
 
 public class taskActivity extends AppCompatActivity{
+
+    private  Moodle moodleUser;
+    private ListView taskList;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
 
-        //test array
-        String[] a = {"a", "b", "c"};
-        List<Assignment> assignmentList = m;
+        moodleUser = moodleInstance.getInstance();
+        taskList = (ListView) findViewById(R.id.listView);
 
-        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a);
+        // Create a progress bar to display while the list loads
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        progressBar.setIndeterminate(true);
+        taskList.setEmptyView(progressBar);
+
+        // Must add the progress bar to the root of the layout
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(progressBar);
+
+
+        //test array
+        List<String> assignmentname = new LinkedList<>();
+        List<Assignment> assignmentList = moodleUser.getAllAssignments();
+
+        for (Assignment assign : assignmentList) {
+           String s = assign.getName();
+            assignmentname.add(s);
+        }
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, assignmentname);
         ListView taskList = (ListView) findViewById(R.id.listView);
         taskList.setAdapter(adp);
+        //taskList.
     }
 }
