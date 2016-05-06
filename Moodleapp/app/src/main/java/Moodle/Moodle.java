fe53375f8532c9057faffdcbb772f53c9cff8a57
levinnir93 <levinnir93@gmail.com>
2016-05-06 15:16:55 +0300

    changes to Moodle login

--- Moodleapp/app/src/main/java/Moodle/Moodle.java
+++ Moodleapp/app/src/main/java/Moodle/Moodle.java
@@ -7,17 +7,35 @@ import java.util.List;
  */
 public class Moodle {
 
-    // Fields will go here
+    private Course[] courses; // all courses
+    private boolean signedIn;
 
     public void Login(String username, String password) {
+        if (!signedIn) {
+            String URL = "http://moodletest.com/login/token.php?username=" + username + "&password=" + password + "&service=moodle_mobile_app");
+            JSONObject json = readJsonFromUrl(URL);
 
+        }
     }
 
     public void Logout() {
-
+        courses = null;
+        signedIn = false;
     }
 
     public List<Assignment> getAllAssignments() {
         return null;
+    }
+
+    private static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 }
